7c0a48e5a302043161efc178cfb8e91cb5ac17f4
RedBlueBird <jiefu.zhang1226@gmail.com>
2025-11-14 21:43:01 +0900

    fixed build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -126,32 +126,32 @@ const symphonyPro = localFont({
 const itcBenguiat = localFont({
   src: [
     {
-      path: "./../../public/fonts/itc-benguiat-std/ITCBenguiatStdBookCn.otf",
+      path: "../../public/fonts/ITC-Benguiat-Std/ITCBenguiatStdBookCn.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./../../public/fonts/itc-benguiat-std/ITCBenguiatStdBookCnIt.otf",
+      path: "../../public/fonts/ITC-Benguiat-Std/ITCBenguiatStdBookCnIt.otf",
       weight: "400",
       style: "italic",
     },
     {
-      path: "./../../public/fonts/itc-benguiat-std/ITCBenguiatStdMediumCn.otf",
+      path: "../../public/fonts/ITC-Benguiat-Std/ITCBenguiatStdMediumCn.otf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./../../public/fonts/itc-benguiat-std/ITCBenguiatStdMediumCnIt.otf",
+      path: "../../public/fonts/ITC-Benguiat-Std/ITCBenguiatStdMediumCnIt.otf",
       weight: "500",
       style: "italic",
     },
     {
-      path: "./../../public/fonts/itc-benguiat-std/ITCBenguiatStdBoldCn.otf",
+      path: "../../public/fonts/ITC-Benguiat-Std/ITCBenguiatStdBoldCn.otf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./../../public/fonts/itc-benguiat-std/ITCBenguiatStdBoldCnIt.otf",
+      path: "../../public/fonts/ITC-Benguiat-Std/ITCBenguiatStdBoldCnIt.otf",
       weight: "700",
       style: "italic",
     },
