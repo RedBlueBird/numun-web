9d0cc59af1178495185772e24c70774f4b7c7c9d
RedBlueBird <jiefu.zhang1226@gmail.com>
2026-01-16 20:08:25 +0900

    updated teams ts

--- src/data/team.ts
+++ src/data/team.ts
@@ -7,7 +7,12 @@ export const teamMembers: TeamMember[] = [
     role: "secretary-general",
     affiliation: "B4 Agricultural Sciences 農学部",
     photo: "/images/team/secretary_general.webp",
-    greeting: `TBD`,
+    greeting: `Hello to all!\n
+    I'm Uyen, and I'm incredibly honored to serve as the NUMUN 2026 Secretary-General this year! I'm excited for us to keep the momentum going and make next year's NUMUN another wonderful success!\n
+    Stepping into this role means so much to me, and I'm truly grateful for the trust and support I've received from all the members. NUMUN has always been a space where dedication, creativity, and collaboration thrive, and I can't wait to help build on that foundation.\n
+    Over the coming year, I hope to work closely with all of you, delegates, directors, staff, and supporters, to create a conference that is not only organized and impactful, but also inspiring, inclusive, and genuinely memorable. Whether you're a seasoned participant or joining us for the very first time, I want NUMUN 2026 to be a place where you feel empowered, challenged, and welcomed.\n
+    Here's to a year of growth, teamwork, and exciting new possibilities! 
+    `,
   },
   {
     id: "dsg-1",
@@ -23,7 +28,12 @@ export const teamMembers: TeamMember[] = [
     role: "deputy-secretary-general",
     affiliation: "B4 Agricultural Sciences 農学部",
     photo: "/images/team/deputy_general2.webp",  
-    greeting: `TBD`,
+    greeting: `A warm welcome to everyone!\n
+    My name is Reika, and it is truly an honor to be able to invite you to NUMUN 2026 as one of your DSGs.\n
+    Since its establishment, NUMUN has provided delegates with the opportunity to explore global issues, develop public speaking and debate skills, and learn collaborative problem-solving through our annual conferences. It has been inspiring to see this tradition continue, and I look forward to making a contribution to its growth.\n
+    As we prepare for the coming year, I hope to take part in building a supportive and organized environment for everyone involved. Through the space we provide, I am eager to see a conference that brings about many meaningful conversations, ideas, and connections.\n
+    I look forward to working with all of you in bringing NUMUN 2026 to its full potential.
+    `,
   },
   {
     id: "oc-1",
@@ -32,34 +42,34 @@ export const teamMembers: TeamMember[] = [
     affiliation: "Committee Leadership",
     photo: "/images/team/dais_team.webp",    
     members: "Gabriel (B4), Uyen (B3), Reika (B4)",
-    description: "TBD",
+    description: "The Dais team is responsible for the overall coordination and management of the conference. They ensure smooth operations and effective communication between all committees.",
   },
   {
     id: "oc-2",
     name: "Academics",
     role: "organizing-committee",
     affiliation: "Substantive Content & Research",
-    photo: "/images/team/itdesign_team.webp",
+    photo: "/images/team/academics_team.webp",
     members: "Dudin (M1), Rachel (B2), Manka (B2), Gab (B4), Clarissa (B4), Jeffrey (B3)",
-    description: "TBD",
+    description: "The academics team handles the procedures and participants of the event. From facilitating NUMUN via topic decision, Chair recruitment and screening, and delegate documents, to preparing workshops and events in preparation, our goal is to provide accurate and engaging debate for all participants each year.",
   },
   {
     id: "oc-3",
     name: "External Affairs",
     role: "organizing-committee",
     affiliation: "Outreach & Delegation Relations",
-    photo: "/images/team/itdesign_team.webp",
+    photo: "/images/team/externalaffairs_team.webp",
     members: "Dudin (M1), Rachel (B2), Manka (B2), Gab (B4), Clarissa (B4), Jeffrey (B3)",
-    description: "TBD",
+    description: "The External Affairs Team coordinates information flow and forges partnerships that power NUMUN 2026. Serving as the primary point of contact, we help ensure smooth communication between conference participants and the internal team. We also conduct formal outreach with educational institutions and media outlets in support of the Marketing Team. From managing delegate inquiries to driving external outreach, we are always working behind the scenes to keep NUMUN connected and growing.",
   },
   {
     id: "oc-4",
     name: "Finance",
     role: "organizing-committee",
     affiliation: "Financial Oversight & Treasury",
-    photo: "/images/team/itdesign_team.webp",
-    members: "Dudin (M1), Rachel (B2), Manka (B2), Gab (B4), Clarissa (B4), Jeffrey (B3)",
-    description: "TBD",
+    photo: "/images/team/finance_team.webp",
+    members: "Jessica (B2), Uyen (B3), Rachel (B2), Tiara (B4)",
+    description: "The Finance Team handles everything from budgeting and financial planning to resource management and creative problem-solving. Our mission is to make sure every idea, project, and committee has the support it needs to shine. Whether it’s crunching numbers, coordinating logistics, or finding efficient solutions, we’re always working behind the scenes to keep the momentum strong.",
   },
   {
     id: "oc-5",
