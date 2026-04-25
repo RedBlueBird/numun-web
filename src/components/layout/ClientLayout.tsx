f0a53a4725ec27c8e2cea157b8c4cc5bb47ce214
RedBlueBird <jiefu.zhang1226@gmail.com>
2026-04-25 18:48:55 +0900

    fix header component not sticky

--- src/components/layout/ClientLayout.tsx
+++ src/components/layout/ClientLayout.tsx
@@ -13,7 +13,7 @@ export default function ClientLayout({ children }: ClientLayoutProps) {
   return (
     <LanguageProvider>
       <Header />
-      <main className="min-h-screen overflow-x-hidden">
+      <main className="min-h-screen">
         {children}
       </main>
       <Footer />
